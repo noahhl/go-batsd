0e4adf41b2aad9f6256fddf49345795da757fd90
Noah Lorang <noah@datarobots.com>
2013-03-06 06:12:28 -0500

    Some clients use an @ for sample size

--- receiver/receiver.go
+++ receiver/receiver.go
@@ -178,7 +178,7 @@ func parseDatapoint(metric string) Datapoint {
 		if len(latter_components) >= 2 {
 			value, _ := strconv.ParseFloat(latter_components[0], 64)
 			if len(latter_components) == 3 && latter_components[1] == "c" {
-				sample_rate, _ := strconv.ParseFloat(latter_components[2], 64)
+				sample_rate, _ := strconv.ParseFloat(strings.Replace(latter_components[2], "@", "", -1), 64)
 				value = value / sample_rate
 			}
 			d = Datapoint{time.Now(), components[0], value, latter_components[1]}
