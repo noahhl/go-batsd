b7b68e459276cafe2bb8631deb5f696fcea61f46
Noah Lorang <lorang@gmail.com>
2014-03-29 08:12:14 -0400

    Make the first tick come early, rather than late

--- gobatsd/heartbeat.go
+++ gobatsd/heartbeat.go
@@ -8,6 +8,7 @@ func NewTickerWithOffset(frequency time.Duration, offset time.Duration) chan tim
 	ch := make(chan time.Time)
 	go func() {
 		time.Sleep(offset)
+		ch <- time.Now()
 		c := time.Tick(frequency)
 		for now := range c {
 			ch <- now
